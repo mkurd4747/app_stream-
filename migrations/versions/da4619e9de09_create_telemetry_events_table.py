6856b1b42d3b8b0e986ca85d89eb96e5ecbe04d1
mkhurd4747 <mkurd4747@gamil.com>
2026-09-08 12:33:31 -0500

    Add request logging and request IDs

--- migrations/versions/da4619e9de09_create_telemetry_events_table.py
+++ migrations/versions/da4619e9de09_create_telemetry_events_table.py
@@ -1,10 +1,11 @@
 """Create telemetry events table
 
 Revision ID: da4619e9de09
-Revises: 
+Revises:
 Create Date: 2026-09-03 14:11:22.823131
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,7 +13,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'da4619e9de09'
+revision: str = "da4619e9de09"
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
